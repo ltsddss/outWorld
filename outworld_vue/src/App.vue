21bc7afd488efdb55785fb276b35bc66e198b16e
lts <419253381@qq.com>
2022-11-24 08:49:16 +0800

    实现在360极速浏览器播放rtmp视频流

---
+++ outworld_vue/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+    </nav>
+    <router-view/>
+  </div>
+</template>
---
+++ outworld_vue/src/views/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <el-form ref="sysUser" v-model="sysUser">
+      <el-form-item lable="用户名">
+        <el-input v-model="sysUser.userName"></el-input>
+      </el-form-item>
+      <el-form-item lable="密码">
+        <el-input v-model="sysUser.userPassword"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      sysUser: {
+        userName: '',
+        userPassword: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      // 发送登录请求
+      this.$axios({
+        url: '/api/outworld_auth/auth/login',
+        method: 'post',
+        data: this.sysUser
+      }).then(r => {
+        // 返回结果
+        console.log(r.data.data)
+        this.$router.push('/index')
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
